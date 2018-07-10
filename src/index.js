7f6101d086ed81f3f5d1c243b5cc852af8db7b0c
avanijagdale <avani.jagdale@gmail.com>
2018-07-10 16:54:10 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Greeting } from "./greeting.js";
+
+class Body extends React.Component{
+  getname(){
+    var name;
+    name = prompt("Enter your name", "");
+    return name;
+  }
+  render (){
+    return (
+      <div>
+        <Greeting name={this.getname()}/>
+      </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(<Body />,document.getElementById('root'));
